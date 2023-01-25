718763d62123152abf9fed82b353d6dc3e2fb7c9
mazuh <marcell-mz@hotmail.com>
2023-01-25 13:16:46 -0300

    feat: chat features

--- README.md
+++ README.md
@@ -3,11 +3,31 @@
 
 > Very simple video conference app using AWS Chime.
 
-For setting up, make sure you have **Node 16** and a AWS profile with Chime access configured using your AWS CLI. Then download the dependencies and run the development server with this command:
+For setting up, make sure you have **Node 16** and a **AWS profile with Chime access configured** using your AWS CLI. Then download the dependencies and run the development server with this command:
 
 ```sh
 npm install
+```
+
+Generate an env file, a copy from `sample.env` but called `.env`, like this:
+
+```sh
+cp sample.env .env
+```
+
+Generate an ARN for your application, here's an example using your configured AWS CLI:
+
+```sh
+aws chime-sdk-identity create-app-instance --name simple-chime-app
+```
+
+Take note of the outputted ARN and use it to fill that `.env` file you just created, along with
+your AWS credentials.
+
+And then run the development server:
+
+```sh
 npm start
 ```
 
-Now make [the frontend](https://github.com/WebRTCventures/simple-chime-frontend) work too after this.
+Now go make [the frontend](https://github.com/WebRTCventures/simple-chime-frontend) work too after this.
--- routes/chime-integration.js
+++ routes/chime-integration.js
@@ -7,8 +7,7 @@ module.exports = async function (fastify, opts) {
     "/chime-integration/meeting-session",
     async function (request, reply) {
       // Initialize Chime instance
-      const chime = new AWS.Chime({ region: "us-east-1" });
-      chime.endpoint = new AWS.Endpoint("https://service.chime.aws.amazon.com");
+      const chime = new AWS.Chime({ region: process.env.AWS_REGION });
 
       // Retrieve Meetings list
       const meetingsResult = await chime.listMeetings().promise();
@@ -24,7 +23,7 @@ module.exports = async function (fastify, opts) {
         (await chime
           .createMeeting({
             ClientRequestToken: uuid(),
-            MediaRegion: "us-west-2",
+            MediaRegion: process.env.AWS_REGION,
             ExternalMeetingId: request.query.room,
           })
           .promise());
@@ -35,10 +34,11 @@ module.exports = async function (fastify, opts) {
         : createdMeetingResponse;
 
       // Create Attendee info using the existing Meeting info.
+      const userUid = uuid();
       const attendeeResponse = await chime
         .createAttendee({
           MeetingId: meetingResponse.Meeting.MeetingId,
-          ExternalUserId: uuid(), // Link the attendee to an identity managed by your application.
+          ExternalUserId: userUid, // Link the attendee to an identity managed by your application.
         })
         .promise();
 
@@ -49,4 +49,115 @@ module.exports = async function (fastify, opts) {
       };
     }
   );
+
+  fastify.get(
+    "/chime-integration/messaging-session/:meetingId",
+    async function (request, reply) {
+      // initialize chime instance
+      const chime = new AWS.Chime({ region: process.env.AWS_REGION });
+
+      // create user identity
+      const userUid = uuid();
+      const idAppInstanceUserResponse = await chime
+        .createAppInstanceUser({
+          AppInstanceArn: process.env.APP_INSTANCE_ARN,
+          AppInstanceUserId: userUid,
+          Name: userUid, // A display name for the user
+        })
+        .promise();
+      const appInstanceUserArn = idAppInstanceUserResponse.AppInstanceUserArn;
+
+      // create chime endpoint
+      const endpointResponse = await chime
+        .getMessagingSessionEndpoint()
+        .promise();
+
+      // find or create selected channel
+      const { meetingId } = request.params;
+
+      async function findChannel() {
+        const listChannelsRequest = await chime.listChannels({
+          AppInstanceArn: process.env.APP_INSTANCE_ARN,
+        });
+        listChannelsRequest.on("build", () => {
+          listChannelsRequest.httpRequest.headers["x-amz-chime-bearer"] =
+            appInstanceUserArn;
+        });
+        const existingChannelsResponse = await listChannelsRequest.promise();
+        return existingChannelsResponse.Channels.find(
+          (c) => c.Name === meetingId
+        );
+      }
+
+      async function createChannel() {
+        const msgChannelCreateRequest = chime.createChannel({
+          AppInstanceArn: process.env.APP_INSTANCE_ARN,
+          Metadata: JSON.stringify({ ChannelType: "PUBLIC_STANDARD" }),
+          Name: meetingId,
+          Mode: "UNRESTRICTED",
+          Privacy: "PUBLIC",
+          ChimeBearer: appInstanceUserArn,
+        });
+        msgChannelCreateRequest.on("build", () => {
+          msgChannelCreateRequest.httpRequest.headers["x-amz-chime-bearer"] =
+            appInstanceUserArn;
+        });
+        return await msgChannelCreateRequest.promise();
+      }
+
+      const msgChannelArn = ((await findChannel()) || (await createChannel()))
+        .ChannelArn;
+
+      // combine channel and user identity
+      async function createChannelMembership() {
+        const msgChannelMembershipRequest = chime.createChannelMembership({
+          ChannelArn: msgChannelArn,
+          MemberArn: appInstanceUserArn,
+          Type: "DEFAULT",
+        });
+        msgChannelMembershipRequest.on("build", () => {
+          msgChannelMembershipRequest.httpRequest.headers[
+            "x-amz-chime-bearer"
+          ] = appInstanceUserArn;
+        });
+        return await msgChannelMembershipRequest.promise();
+      }
+      const msgChannelMembershipResponse = await createChannelMembership();
+
+      // Respond with these infos so the frontend can safely use it
+      return {
+        msgChannelArn,
+        msgChannelMembershipResponse,
+        endpointResponse,
+        region: process.env.AWS_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      };
+    }
+  );
+
+  fastify.post("/chime-integration/message", async function (request, reply) {
+    const { channelMembership, content } = request.body;
+
+    const chime = new AWS.Chime({ region: process.env.AWS_REGION });
+
+    const channelMesssageRequest = chime.sendChannelMessage({
+      ChannelArn: channelMembership.ChannelArn,
+      Content: content,
+      Persistence: "NON_PERSISTENT",
+      Type: "STANDARD",
+    });
+    channelMesssageRequest.on("build", () => {
+      channelMesssageRequest.httpRequest.headers["x-amz-chime-bearer"] =
+        channelMembership.Member.Arn;
+    });
+    const channelMesssageRresponse = await channelMesssageRequest.promise();
+
+    const sentMessage = {
+      response: channelMesssageRresponse,
+      CreatedTimestamp: new Date(),
+      Sender: { Arn: channelMembership.Member.Arn, Name: channelMembership },
+    };
+    return sentMessage;
+  });
 };
